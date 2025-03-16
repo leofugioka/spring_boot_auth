6163c608a05ac56d528c03e68ad956a47bf9174a
Leandro Fugioka <77177218+leofugioka@users.noreply.github.com>
2025-03-16 16:46:47 -0300

    Initial project files

---
+++ src/main/java/com/springauth/springauthproject/SpringauthprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springauth.springauthproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringauthprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringauthprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springauth/springauthproject/SpringauthprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springauth.springauthproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringauthprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
